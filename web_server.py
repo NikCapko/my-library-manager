73ab9322e947823aecb19bbd3d38d2d9cf3e1dcd
Nikolay <nik.capko@mail.ru>
2025-08-16 17:38:07 +0300

    add mobile size to books list page

--- web_server.py
+++ web_server.py
@@ -18,6 +18,7 @@ BASE_HTML = """
 <html>
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Библиотека</title>
     <style>
         body { font-family: sans-serif; margin: 20px; }
